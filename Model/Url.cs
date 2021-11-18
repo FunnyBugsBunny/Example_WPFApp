91b5f96f218a4b7512c55528502f85fb448dc2ab
Дмитрий Карагодин <fastrap96@gmail.com>
2021-11-18 12:14:41 +0500

    Добавьте файлы проекта.

---
+++ Model/Url.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuipuTest.Model
+{
+    public class Url
+    {
+        public string strUrl;
+        public int numberTagA;
+        public Url(string strUrl)
+        {
+            this.strUrl = strUrl;
+            this.numberTagA = CalcNumberTagA();
+        }
+        private static int CalcNumberTagA()
+        {
+            int number = 0;
+            try
+            {
+                //using ()
+                //{
+
+                //}
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return number;
+        }
+    }
+}
